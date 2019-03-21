7ad1d3cd22f97eb2e96df9460eb2dea8b1234d99
heiligbasil <basilief@yahoo.com>
2019-03-21 14:55:29 -0700

    Part 4 is done - Layout has been built

--- app/src/main/java/com/lambdaschool/android_mvc_applist/MainActivity.java
+++ app/src/main/java/com/lambdaschool/android_mvc_applist/MainActivity.java
@@ -3,6 +3,8 @@ package com.lambdaschool.android_mvc_applist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static com.lambdaschool.android_mvc_applist.AppRepository.getAppListings;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        getAppListings();
     }
 }
