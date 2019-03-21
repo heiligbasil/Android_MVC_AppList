502eb2d167a50b7c017b136d0f01955f78024c84
heiligbasil <basilief@yahoo.com>
2019-03-21 13:22:40 -0700

    Part 2 is done - Data Model Class is built

---
+++ app/src/main/java/com/lambdaschool/android_mvc_applist/AppListing.java
@@ -0,0 +1,55 @@
+package com.lambdaschool.android_mvc_applist;
+
+public class AppListing {
+
+    private  int id;
+    private String appName, appVersion, domainName, contactEmail, imageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public void setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
